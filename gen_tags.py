e1479bf92858dd24b668278eff970412d1813e99
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2016-01-30 14:38:27 -0500

    Improve tags generator.

--- gen_tags.py
+++ gen_tags.py
@@ -6,12 +6,38 @@ Do NOT edit the tags.json file.
 """
 from __future__ import absolute_import
 
+import argparse
+from argparse import RawDescriptionHelpFormatter as RawDescriptionHelp
 import json
 import os
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 if os.path.dirname(__file__) == '':
     ROOT = os.getcwd()
+
+
+def create_args_parser():
+    """
+    Create argument parser.
+    """
+    prog_name = os.path.basename(__file__)[0:-3]
+    mesg = """
+    Generate the tags.json database that maps
+    tags onto plugin names.
+
+    Do NOT edit the tags.json file.
+    To update tags, edit db.json and regenerate.
+    """.format(prog_name.capitalize(), prog_name)
+    mesg = mesg[0:-5]
+    parser = argparse.ArgumentParser(prog=prog_name, description=mesg,
+                                     formatter_class=RawDescriptionHelp)
+    parser.add_argument('-i', '--input',
+                        default=os.path.join(ROOT, 'db.json'),
+                        help='input json file to transform into tags')
+    parser.add_argument('-o', '--output',
+                        help='output json, by default tags.json in input dir')
+
+    return parser
 
 
 def add_tags(tags, plugin, plugin_tags):
@@ -28,15 +54,21 @@ def main():
     """
     Main entry point.
     """
-    with open(os.path.join(ROOT, 'db.json'), 'r') as fin:
+    parser = create_args_parser()
+    args = parser.parse_args()
+    if args.output is None:
+        args.output = os.path.join(os.path.dirname(args.input), 'tags.json')
+
+    with open(args.input, 'r') as fin:
         plugs = json.load(fin)
 
     tags = {}
     for plug in plugs:
         add_tags(tags, plug, plugs[plug]['tags'])
 
-    with open(os.path.join(ROOT, 'tags.json'), 'w') as fout:
-        json.dump(tags, fout, sort_keys=True, indent=2, separators=(',', ': '))
+    with open(args.output, 'w') as fout:
+        json.dump(tags, fout, sort_keys=True,
+                  indent=2, separators=(',', ': '))
 
 
 if __name__ == "__main__":
